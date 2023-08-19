d252b6b2a4dc104f5008510b0eef9e6cac426450
rusnak <rusnak1997@gmail.com>
2023-08-19 13:23:19 +0300

    Created a separate Directory component and a CategoryItem component for distinct categories

--- src/App.js
+++ src/App.js
@@ -1,40 +1,35 @@
+import { Directory } from './components';
+
 const App = () => {
   const categories = [
     {
       id: 1,
       title: 'Hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
     },
     {
       id: 2,
       title: 'Jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
     },
     {
       id: 3,
       title: 'Sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
     },
     {
       id: 4,
       title: 'Women',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
     },
     {
       id: 5,
       title: 'Men',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
     },
   ];
 
-  return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <div className="category-container" key={category.id}>
-          <div className="background-image" />
-          <div className="category-body-container">
-            <h2>{category.title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
