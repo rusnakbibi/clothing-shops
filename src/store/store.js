0cfdf18138a8e1fbcb10747fbca649138fa51584
rusnak <rusnak1997@gmail.com>
2024-01-26 19:25:22 +0200

    Creating custom logger middleware and adjusting Redux DevTools

--- src/store/store.js
+++ src/store/store.js
@@ -11,7 +11,9 @@ import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
-const middleWares = [logger];
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(
+  Boolean
+);
 
 const persistConfig = {
   key: 'root',
@@ -21,7 +23,13 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const composeEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composeEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composeEnhancers);
 
